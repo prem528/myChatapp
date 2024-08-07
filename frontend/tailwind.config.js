4daf05dc8926b2a696c31cd6a916bdaaef3b122f
Prem Yadav <116069911+prem528@users.noreply.github.com>
2024-08-07 11:15:43 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+ /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '128': '32rem',  
+        '144': '36rem',  
+      },
+      maxHeight: {
+        '128': '32rem', 
+        '144': '36rem',  
+      },
+    },
   },
   plugins: [
     require('daisyui'),
